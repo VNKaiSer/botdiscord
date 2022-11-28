edb28c26c2ec432dd0cbb1e16a38792bedf1258b
kaiser <tandatvok16@gmail.com>
2022-11-28 09:49:39 +0700

    f

--- slash/queue.js
+++ slash/queue.js
@@ -1,24 +1,24 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
+const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("queue")
-    .setDescription("displays the current song queue")
-    .addNumberOption((option) => option.setName("page").setDescription("Page number of the queue").setMinValue(1)),
+        .setName("queue")
+        .setDescription("Hiển thị bài hát ở hàng chờ")
+        .addNumberOption((option) => option.setName("page").setDescription("Page number of the queue").setMinValue(1)),
 
-    run: async ({ client, interaction }) => {
+    run: async({ client, interaction }) => {
         const queue = client.player.getQueue(interaction.guildId)
-        if (!queue || !queue.playing){
-            return await interaction.editReply("There are no songs in the queue")
+        if (!queue || !queue.playing) {
+            return await interaction.editReply("Không có bài hát nào ở đây đou")
         }
 
         const totalPages = Math.ceil(queue.tracks.length / 10) || 1
         const page = (interaction.options.getNumber("page") || 1) - 1
 
-        if (page + 1 > totalPages) 
+        if (page + 1 > totalPages)
             return await interaction.editReply(`Invalid Page. There are only a total of ${totalPages} pages of songs`)
-        
+
         const queueString = queue.tracks.slice(page * 10, page * 10 + 10).map((song, i) => {
             return `**${page * 10 + i + 1}.** \`[${song.duration}]\` ${song.title} -- <@${song.requestedBy.id}>`
         }).join("\n")
@@ -27,15 +27,15 @@ module.exports = {
 
         await interaction.editReply({
             embeds: [
-                new MessageEmbed()
-                    .setDescription(`**Currently Playing**\n` + 
+                new EmbedBuilder()
+                .setDescription(`**Bài đang phát**\n` +
                     (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : "None") +
                     `\n\n**Queue**\n${queueString}`
-                    )
-                    .setFooter({
-                        text: `Page ${page + 1} of ${totalPages}`
-                    })
-                    .setThumbnail(currentSong.setThumbnail)
+                )
+                .setFooter({
+                    text: `Page ${page + 1} of ${totalPages}`
+                })
+                .setThumbnail(currentSong.setThumbnail)
             ]
         })
     }
